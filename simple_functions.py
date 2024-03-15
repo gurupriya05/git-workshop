bda80794da1d0cd5f9fe4996726430fa9c35317e
Dhikshita <dhikshita.macher@tigeranalytics.com>
2024-03-15 14:51:49 +0530

    Issue 2 Commit(rebased)

--- simple_functions.py
+++ simple_functions.py
@@ -2,8 +2,13 @@
 
 def double_number(a):
 '''This function takes the input (a) which is a number and prints the number before and after doubling it and returns the doubled number (a+a)'''
+    print(f'value before double_number(): {a}')
+    print(f'value after double_number(): {a+a}')
     return a+a
+    
 
 def square_number(a):
 '''This function takes the input (a) which is a number and prints the original number and the square of it and returns the squared number (a*a)'''
+    print(f'value before square_number(): {a}')
+    print(f'value after square_number(): {a*a}')
     return a*a
