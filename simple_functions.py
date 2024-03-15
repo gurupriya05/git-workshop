45bb45751928a67391cca4cdde8af1fd5d4ee5cd
gurupriya05 <gurupriya.e@tigeranalytics.com>
2024-03-15 13:34:25 +0530

    Added docstrings for the file simple_functions.py

--- simple_functions.py
+++ simple_functions.py
@@ -1,7 +1,9 @@
 # Custom python functions
 
 def double_number(a):
+'''This function takes the input and prints the number before and after doubling it and returns the doubled number'''
     return a+a
 
 def square_number(a):
+'''This function takes the input and prints the original number and the square of it and returns the squared number'''
     return a*a
